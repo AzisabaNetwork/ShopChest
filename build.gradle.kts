cfccb1952879acebe6aeabb54e563731ca5a578e
albardoo02 <akshs@outlook.jp>
2026-07-31 23:57:51 +0900

    chore: bump version to 1.21.11+1.14.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "de.epiceric"
-version = "1.21.11+1.13.9"
+version = "1.21.11+1.14.0"
 description = "ShopChest"
 
 val projectUrl = project.findProperty("projectUrl")?.toString().orEmpty()
